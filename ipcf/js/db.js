29fd1e00f68a9f3d132c1cfd61ac66f0119ba5ea
rmnakash <rmnakash@gmail.com>
2017-08-31 10:50:47 +0530

    Akash Ramani
    Test

---
+++ ipcf/js/db.js
@@ -0,0 +1,106 @@
+/**
+ * Created by : Akash Ramani
+   Creation Date : 30-08-2017
+ */
+
+var fixvalue =
+			{
+			  "ID" : 1,
+			  "B16_1":6.75,
+			  "B19_1": 15,
+			  "B21_1": 30,
+			  "B23_1":225000,
+			  "B44_2": 60,
+			  "B45_2": 20,
+			  "B46_2": 30.9,
+			  "B51_2": 5,
+			  "B52_2": 300
+			}; 
+
+var fuelprice = 
+		[{
+	      "ID" : 1,
+		  "Fuel":"Furnace Oil",
+		  "GCV": 9800,
+		  "Indicative_Price": 16,
+		  "RsKg":0,
+		  "Per_unit": 0,
+		  "RsLtr": 15.2
+	    },
+	     {
+	      "ID" : 2,
+		  "Fuel":"HSD",
+		  "GCV": 14723,
+		  "Indicative_Price": 65,
+		  "RsKg":0,
+		  "Per_unit": 0,
+		  "RsLtr": 25.5
+	    },
+	    {
+	      "ID" : 3,
+		  "Fuel":"Natural Gas",
+		  "GCV": 10625,
+		  "Indicative_Price": 45,
+		  "RsKg":0,
+		  "Per_unit": 0,
+		  "RsLtr": 0
+	    },
+	    {
+	      "ID" : 4,
+		  "Fuel":"LDO",
+		  "GCV": 10500,
+		  "Indicative_Price": 36,
+		  "RsKg":0,
+		  "Per_unit": 0,
+		  "RsLtr": 0
+	    },
+	    {
+	      "ID" : 5,
+		  "Fuel":"Wood",
+		  "GCV": 2200,
+		  "Indicative_Price": 3,
+		  "RsKg":0,
+		  "Per_unit": 0,
+		  "RsLtr": 0
+	    },
+	    {
+	      "ID" : 6,
+		  "Fuel":"Coal",
+		  "GCV": 5000,
+		  "Indicative_Price": 3.5,
+		  "RsKg":0,
+		  "Per_unit": 0,
+		  "RsLtr": 0
+	    },
+	    {
+	      "ID" : 7,
+		  "Fuel":"Electricity",
+		  "GCV": 0,
+		  "Indicative_Price": 7,
+		  "RsKg":0,
+		  "Per_unit": 0,
+		  "RsLtr": 0
+	    },
+	    {
+	      "ID" : 8,
+		  "Fuel":"Diesel",
+		  "GCV": 10800,
+		  "Indicative_Price": 62.18,
+		  "RsKg":0,
+		  "Per_unit": 0,
+		  "RsLtr": 52.85
+	    }];
+
+var wateroil = 
+[
+{
+  "ID" : 1,
+  "Fluid":"Water",
+  "SpecificHeat": 4.186
+},
+{
+  "ID" : 2,
+  "Fluid":"Oil",
+  "SpecificHeat": 2.5
+}
+];
